bbf769f097a04f1ee9bb930df895fb87cffbf385
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-03 14:30:55 +0200

    Remove pragma's

--- main_test.cpp
+++ main_test.cpp
@@ -1,5 +1,5 @@
 // Boost.Test does not play well with -Weffc++
-#pragma GCC diagnostic push
+
 
 
 #define BOOST_TEST_DYN_LINK // Defines a main function
@@ -7,4 +7,4 @@
 #define BOOST_TEST_MODULE test_reversi_module
 #include <boost/test/unit_test.hpp>
 
-#pragma GCC diagnostic pop
+
